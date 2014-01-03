9c969faa4d16ffb83efa09532ef8d7ca54621e30
bradshoow <mat.simonnet@gmail.com>
2014-01-03 14:24:25 +0100

    landscape mode defined

--- src/fr/irit/geotablet_interactions/layout_t12/MainActivity.java
+++ src/fr/irit/geotablet_interactions/layout_t12/MainActivity.java
@@ -1,11 +1,11 @@
 package fr.irit.geotablet_interactions.layout_t12;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Menu;
-
+import android.view.View;
 import fr.irit.layout_t12.R;
-
 import fr.irit.geotablet_interactions.common.MyMapView;
 import fr.irit.geotablet_interactions.common.MyTTS;
 
@@ -17,6 +17,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+    	//set Full screen landscape
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		
+		//hide actionBar (up) -> does not work on galaxyTab 10.1
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
+				
+		//hide menuBar (bottom)
+		//this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
 
 		mapView = (MyMapView) findViewById(R.id.map_view);
 
